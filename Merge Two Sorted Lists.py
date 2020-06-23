0501b052ee30f84be46c8b3f1a7740a3da5d95bf
Dat Nguyen <tiendat.ams@gmail.com>
2020-06-22 23:17:40 -0700

    Create Merge Two Sorted Lists.py

---
+++ Merge Two Sorted Lists.py
@@ -0,0 +1,12 @@
+l1 = [2, 4, 6, 7]
+l2 = [1, 2, 3]
+
+lall = l1 + l2
+print (sorted(lall))
+#%%
+def mergeTwoLists(l1, l2):
+    lall = l1 + l2
+    return print(sorted(lall))
+l1 = [2, 4, 6, 7]
+l2 = [1, 2, 3]
+mergeTwoLists (l1,l2)
