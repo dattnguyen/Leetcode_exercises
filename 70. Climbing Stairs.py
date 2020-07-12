e4a93a58d56bc5ee853bcb9a30b5c9e0b2e9a152
Dat Nguyen <tiendat.ams@gmail.com>
2020-07-11 23:13:02 -0700

    Update 70. Climbing Stairs.py

--- 70. Climbing Stairs.py
+++ 70. Climbing Stairs.py
@@ -3,19 +3,13 @@
 #In how many distinct ways can you climb to the top?
 
 def climbstairs(n):
-    if n == 0:
+    if n == 0: #corner case
         return print(0)
-    if n == 1:
+    if n == 1: #corner case. Also want to start with n = 2
         return print(1)
     hmap = {0 : 1, 1: 1}
-    for i in range(2,n+1):
-        hmap[i] = hmap[i-1] + hmap[i-2]
+    for i in range(2,n+1): #if we take out 1 step, it becomes (n-1) step problem which we already solve
+        hmap[i] = hmap[i-1] + hmap[i-2] #resemblence of fibonacci sequence
     return print(hmap[n])
 
 climbstairs(5)
-#%%
-n = 4
-hmap = {0 : 1, 1: 1}
-for i in range(2, n+1):
-    hmap[i] = hmap[i - 1] + hmap[i - 2]
-print(hmap[n])
