9e1ed7748bed73a871c981ce8ffbf9573f810825
Dat Nguyen <tiendat.ams@gmail.com>
2020-07-18 00:57:02 -0700

    Update 15. 3Sum.py

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -5,5 +5,42 @@
 nums = [-1, 0, 1, 2, -1, -4]
 
 hmap = {}
-
 res = []
+
+for i in range(len(nums)):
+    target = 0 - nums[i]
+    for j, num in enumerate(nums):
+        if j != i:
+            temp = target - num
+            if temp not in hmap:
+                hmap[num] = 1
+            else:
+                res.append([num, temp, nums[i]])
+
+print(res)
+#%%
+nums = [-1, 0, 1, 2, -1, -4]
+
+#idea is a+b = -c => use 2Sum problem to solve 3Sum
+#for duplicate case, we use continue to keep increasing i
+#sort makes everything easier
+nums.sort()
+result = []
+for i in range(len(nums)):
+    if i > 0 and nums[i] == nums[i-1]: #handle duplicate case
+        continue
+    target = 0 - nums[i] #turn this into 2Sum
+    left = i+1
+    right = len(nums)-1
+    while left < right: #sort and then use 2 pointer technique to find a and b
+        if nums[left] + nums[right] == target:
+            result.append([nums[i], nums[left], nums[right]])
+            left += 1
+            while left < right and nums[left] == nums[left - 1]: #handle duplicate case in 2Sum
+                left +=1
+        elif nums[left] + nums[right] > target:
+            right -= 1
+        else:
+            left += 1
+
+print(result)
