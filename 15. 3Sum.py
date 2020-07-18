2d845d5c40857b468c43c02932ffcfac6f293282
Dat Nguyen <tiendat.ams@gmail.com>
2020-07-17 22:27:59 -0700

    Create 15. 3Sum.py

---
+++ 15. 3Sum.py
@@ -0,0 +1,9 @@
+# Given an array nums of n integers, are there elements a, b, c in nums
+# such that a + b + c = 0?
+# Find all unique triplets in the array which gives the sum of zero.
+
+nums = [-1, 0, 1, 2, -1, -4]
+
+hmap = {}
+
+res = []
