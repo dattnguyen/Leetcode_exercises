8123a4fa37d09afb8571a546ec7c89cdfa60c1c2
Dat Nguyen <tiendat.ams@gmail.com>
2020-07-18 00:56:58 -0700

    Update 1. Two_sum.py

--- 1. Two_sum.py
+++ 1. Two_sum.py
@@ -23,3 +23,25 @@ def twoSum(mylist, target):
 mylist = [2, 7, 11, 15, 22, 67]
 target = 24
 twoSum(mylist,target)
+
+#%%
+#sorting and use 2 pointer technique
+nums = [-1, 0, 1, 2, -1, -4, 3, 4, 7, 8, -2, 2]
+target = 5
+
+nums.sort() #sort the array first to use 2 pointer technique
+left = 0
+right = len(nums)-1
+result = []
+
+while left < right: #start from the two ends towards the middle
+    if nums[left] + nums[right] == target:
+        result.append([nums[left], nums[right]])
+        left += 1
+        while left < right and nums[left] == nums[left-1]: #account for duplicate
+            left += 1 #by moving the left pointer pass the duplicate value
+    elif nums[left] + nums[right] > target: #if the sum is more than target, reduce right
+        right -= 1
+    else: #if the sum is less than target, increase left
+        left += 1
+print(result)
